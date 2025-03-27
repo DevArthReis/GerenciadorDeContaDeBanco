e834d00d4bcfa0293c1b91a6f6f81d66d592b6ac
arthur albuquerque <arthuralbuquerque.hsh@gmail.com>
2025-03-27 18:53:22 -0300

    Adicionar arquivos de projeto.

---
+++ GerenciadorDeContaDeBanco/ContaBancaria.cs
@@ -0,0 +1,25 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+using System.Globalization;
+
+namespace GerenciadorDeContaDeBanco
+{
+    internal class ContaBancaria
+    {
+        public string NumeroConta;
+        public string Titular;
+        public double Saldo;
+        public double Deposito;
+        public double Saque;
+        public double Depositar()
+        {
+            return Saldo + Deposito;
+
+        }
+        public double Sacar()
+        {
+            return Saldo - Saque;
+        }
+        
+    }
+
+}
---
+++ GerenciadorDeContaDeBanco/Program.cs
@@ -0,0 +1,26 @@
+﻿using GerenciadorDeContaDeBanco;
+using System.Globalization;
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria Conta = new ContaBancaria();
+            Console.WriteLine("Digite o numero da Conta:");
+            Conta.NumeroConta = Console.ReadLine();
+            Console.WriteLine("Digite o titular:");
+            Conta.Titular = Console.ReadLine();
+            Console.WriteLine("Quanto deseja adicionar ao seu saldo:");
+            Conta.Saldo = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.WriteLine($"O numero da Conta é:{Conta.NumeroConta} o titular é {Conta.Titular} Com o saldo de {Conta.Saldo.ToString("F2",CultureInfo.InvariantCulture)}");
+            Console.WriteLine("Quanto deseja Depositar a essa conta:");
+            Conta.Deposito = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.WriteLine($"O numero da Conta é{Conta.NumeroConta} o titular é {Conta.Titular} Com o saldo de {Conta.Depositar().ToString("F2",CultureInfo.InvariantCulture)}");
+            Console.WriteLine("Quanto deseja sacar a essa conta:");
+            Conta.Saque = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.WriteLine($"O numero da Conta é{Conta.NumeroConta} o titular é {Conta.Titular} Com o saldo de {Conta.Sacar().ToString("F2", CultureInfo.InvariantCulture)}");
+
+        }
+    }
+}
